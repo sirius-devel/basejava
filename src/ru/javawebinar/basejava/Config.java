15b25aafd0ec24d85924e2850145239e37cf641a
sirius-devel <fyodorova.i.v@gmail.com>
2020-08-30 21:50:04 +0300

    deploy to heroku

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -10,20 +10,20 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
     private Properties props = new Properties();
     private File storageDir;
     private final Storage storage;
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"),
                     props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
@@ -37,14 +37,5 @@ public class Config {
 
     public static Config get() {
         return INSTANCE;
-    }
-
-    private static File getHomeDir() {
-        String prop = System.getProperty("homeDir");
-        File homeDir = new File(prop == null ? "." : prop);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir + " is not directory");
-        }
-        return homeDir;
     }
 }
